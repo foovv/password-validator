fc9ab817b91375d3b4283c92128f4bb428a46de3
fov <fovikson@proton.me>
2025-01-30 14:25:58 +0100

    added some animations

--- js/main.js
+++ js/main.js
@@ -2,11 +2,13 @@ const signInForm = document.querySelector('.signin')
 const signUpForm = document.querySelector('.signup')
 const toggleLinks = document.querySelectorAll('.form__text--special')
 
+const changesForm = () => {
+  signInForm.classList.toggle('show')
+  signInForm.classList.toggle('hide')
+  signUpForm.classList.toggle('show')
+  signUpForm.classList.toggle('hide')
+}
+
 toggleLinks.forEach((link) => {
-  link.addEventListener('click', (e) => {
-    signInForm.classList.toggle('show')
-    signInForm.classList.toggle('hide')
-    signUpForm.classList.toggle('show')
-    signUpForm.classList.toggle('hide')
-  })
+  link.addEventListener('click', changesForm)
 })
